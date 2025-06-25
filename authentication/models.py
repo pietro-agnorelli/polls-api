057ce548ec76c305d98e4ab7500aab5910603979
Pietro Agnorelli <pietro.agnorelli@edu.unifi.it>
2025-06-25 10:49:46 +0200

    Added user model for auth app

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from polls.models import Polls
+
 
 # Create your models here.
+
+
+class AuthUser(User):
+    polls = models.ManyToManyField(Polls)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
