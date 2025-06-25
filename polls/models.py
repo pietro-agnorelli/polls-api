2c85e503d529b889d53b7f7052b3b2c14e579ce6
Pietro Agnorelli <pietro.agnorelli@edu.unifi.it>
2025-06-25 15:48:10 +0200

    Added vote model and serializers Modified polls serializer to accomodate creation

--- polls/models.py
+++ polls/models.py
@@ -13,4 +13,12 @@ class Polls(models.Model):
 class Choices(models.Model):
     poll = models.ForeignKey(Polls, related_name='choices', on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=100)
-    votes = models.IntegerField(default=0)
+
+
+class Votes(models.Model):
+    poll = models.ForeignKey(Polls, related_name='votes', on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choices, related_name='votes', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', related_name='votes', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('choice', 'user')  # Ensure a user can vote only once per choice
--- polls/serializers.py
+++ polls/serializers.py
@@ -1,10 +1,35 @@
 from rest_framework import serializers
-from .models import Polls
+from .models import Polls, Choices, Votes
+
+
+class ChoicesSerializer(serializers.ModelSerializer):
+    poll = serializers.PrimaryKeyRelatedField(queryset=Polls.objects.all())
+    class Meta:
+        model = Choices
+        fields = ['id', 'poll', 'choice_text']
 
 
 class PollsSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source='creator.username')
+    choices = ChoicesSerializer(many=True, read_only=True)
 
     class Meta:
         model = Polls
-        fields = ['id', 'question', 'pub_date', 'is_active', 'creator']
+        fields = ['id', 'question', 'is_active', 'creator', 'choices']
+
+    def create(self, validated_data):
+        choices_data = validated_data.pop('choices')
+        poll = Polls.objects.create(**validated_data)
+        for choice_data in choices_data:
+            Choices.objects.create(poll=poll, **choice_data)
+        return poll
+
+
+class VotesSerializer(serializers.ModelSerializer):
+    poll = serializers.PrimaryKeyRelatedField(queryset=Polls.objects.all())
+    choice = serializers.PrimaryKeyRelatedField(queryset=Choices.objects.all())
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = Votes
+        fields = ['id', 'poll', 'choice', 'user']
