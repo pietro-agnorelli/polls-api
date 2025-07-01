d8ef7d682f21e2d52aa590fa59a539a854e9ecbd
Pietro Agnorelli <pietro.agnorelli@edu.unifi.it>
2025-07-01 15:33:57 +0200

    various fixes

--- polls-api/asgi.py
+++ polls-api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for polls_api project.
+ASGI config for polls-api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polls_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polls-api.settings')
 
 application = get_asgi_application()
